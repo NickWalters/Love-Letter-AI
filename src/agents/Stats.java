149d27361e9edb9be3138c6eaaa5abd430412893
Nick Walters <nicho.walters@gmail.com>
2019-10-19 15:59:55 +0800

    Update Stats.java

--- src/agents/Stats.java
+++ src/agents/Stats.java
@@ -17,7 +17,7 @@ public class Stats extends LoveLetter{
     }
 
     public static void getData() {
-        Agent[] agents = {new RandomAgent(),new heuristicAgent(), new Agent22243339(), new Agent21938451()};
+        Agent[] agents = {new RandomAgent(),new Agent22243339(), new heuristicAgent(), new Agent21938451()};
 
         String[] columnName = {"Agent","Win Rate (Out of 100 games)", "Avg score per game"};
         double[][] dataTable = new double[4][3];
