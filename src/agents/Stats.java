38ff93f620ea919a17603bffd6261ed55b06bdd5
Nick Walters <nicho.walters@gmail.com>
2019-10-19 12:42:33 +0800

    Statistics for winning

---
+++ src/agents/Stats.java
@@ -0,0 +1,48 @@
+package loveletter;
+
+import java.util.Arrays;
+
+import agents.Agent_22232803;
+import agents.RandomAgent;
+
+
+public class Stats extends LoveLetter{
+
+    public Stats(){
+
+    }
+
+    public static void getData() {
+        Agent[] agents = {new Agent_22232803(),new Agent_22232803(), new Agent_22232803(), new Agent_22232803()};
+
+        String[] columnName = {"Agent","Win Rate (Out of 100 games)", "Avg score per game"};
+        double[][] dataTable = new double[4][3];
+
+        for(int i = 0; i < 100; i++) {
+            LoveLetter game = new LoveLetter();
+            int[] data = game.playGame(agents);
+
+
+            //Average score
+            for(int d = 0; d <= 3; d++) {
+                dataTable[d][0] = d;
+                if(data[d] == 4) {
+                    dataTable[d][1] += 1;
+                }
+                dataTable[d][2] += data[d];
+            } 
+        }
+
+        for(int e = 0; e <= 3; e++){
+            dataTable[e][1] = dataTable[e][1]/100;
+            dataTable[e][2] = dataTable[e][2]/100;
+        }
+
+        System.out.print(Arrays.toString(columnName) +"\n");
+        System.out.print(Arrays.deepToString(dataTable)+"\n");
+    }
+
+    public static void main(String[] args){
+        Stats.getData();
+    }
+}
